7256d09cacde985619da35484c2183222b9534cb
Emmanuel Tchetche <tchetchelevis6gmail.com>
2024-04-23 12:22:33 +0200

    version avec mail qui passe de manière standard

--- assets/phpscripts/contact.php
+++ assets/phpscripts/contact.php
@@ -1,33 +1,31 @@
 <?php
-$to = "tchetchelevis6@gmail.com"; // Votre adresse e-mail où vous recevrez les messages
-$from = $_POST['email']; // L'adresse e-mail du formulaire
-$first_name = $_POST['name']; // Prénom de l'expéditeur
-$subject = $_POST['subject']; // Objet du message
-$message = $_POST['message']; // Contenu du message
+//this is your Email address
+//kinldy update your email here
+$to = "levi.tchetche@teambi.ci";
+// $from = "tchetchelevis6@gmail.com";
+//this is the sender's Email address
+//this is firt name
+// $first_name = $_POST['name'];
+//this is last name
+// $last_name = $_POST['name'];
+//this is subject
+$subject = "mail test levi";
+//this is message body
+// $message = "Message " . $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+$message = "Bonjour pour un test";
 
-// Configurer les paramètres du serveur SMTP de Mailtrap
-$smtp_server = "smtp.mailtrap.io";
-$smtp_port = 2525;
-$smtp_username = "emmanuel";
-$smtp_password = "Bac2020delevis";
+// $headers = "From:" . $from . "\r\n" .
+//     "Reply-To:" . $from . "\r\n" .
+//     'X-Mailer: PHP/' . phpversion();
+$headers = "Content-Type: text/plain; charset=utf-8\r\n";
 
-$headers = "From:" . $from . "\r\n" .
-    'Reply-To: ' . $from . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$headers .= "From: tchetchelevis6@gmail.com\r\n";
+// mail($from, $subject, $message, $headers2);
+mail($to, $subject, $message, $headers);
 
-// Configurer l'envoi d'e-mail via SMTP
-ini_set("SMTP", $smtp_server);
-ini_set("smtp_port", $smtp_port);
-ini_set("smtp_username", $smtp_username);
-ini_set("smtp_password", $smtp_password);
-
-// Envoi de l'e-mail
-$mail1 = mail($to, $subject, $message, $headers);
-$mail2 = mail($from, $subject, $message, $headers);
-
-if ($mail1 && $mail2) {
-    echo "email bien envoyé"; // Envoi réussi
+if (mail($to, $subject, $message, $headers)) {
+    echo 'email bien envoyé à ' . $to;
 } else {
-    echo "echec de l'envoie"; // Erreur lors de l'envoi
+    echo 'erreur ';
 }
 ?>
