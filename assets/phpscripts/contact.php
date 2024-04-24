a48194116dd115dcb46452a5472889984eb9c878
Emmanuel Tchetche <tchetchelevis6gmail.com>
2024-04-24 18:06:55 +0200

    push avec tout qui marche bien

--- assets/phpscripts/contact.php
+++ assets/phpscripts/contact.php
@@ -1,31 +1,29 @@
 <?php
-//this is your Email address
-//kinldy update your email here
-$to = "levi.tchetche@teambi.ci";
-// $from = "tchetchelevis6@gmail.com";
-//this is the sender's Email address
-//this is firt name
-// $first_name = $_POST['name'];
-//this is last name
-// $last_name = $_POST['name'];
-//this is subject
-$subject = "mail test levi";
-//this is message body
-// $message = "Message " . $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-$message = "Bonjour pour un test";
+    /*  
+        Template name    : Technoit - IT Solutions & Business Services Multipurpose Responsive Website Template
+        Author           : ZRTHEMES
+        Version          : 1.0
+        File Description : Contact PHP file of the template
+    */
+    //this is your Email address
+    //kinldy update your email here
+    $to = "info@teambi.ci"; 
+    $from = $_POST['email']; 
+    //this is the sender's Email address
+    //this is firt name
+    $first_name = $_POST['name'];
+    //this is last name
+    $last_name = $_POST['name'];
+    //this is subject
+    $subject = $_POST['subject'];
+    //this is message body
+    $message = $_POST['message'];
 
-// $headers = "From:" . $from . "\r\n" .
-//     "Reply-To:" . $from . "\r\n" .
-//     'X-Mailer: PHP/' . phpversion();
-$headers = "Content-Type: text/plain; charset=utf-8\r\n";
+    $headers = "From:" . $from . "\r\n" .
+        'Reply-To: webmaster@example.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 
-$headers .= "From: tchetchelevis6@gmail.com\r\n";
-// mail($from, $subject, $message, $headers2);
-mail($to, $subject, $message, $headers);
-
-if (mail($to, $subject, $message, $headers)) {
-    echo 'email bien envoyé à ' . $to;
-} else {
-    echo 'erreur ';
-}
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject,$message,$headers2); 
 ?>
